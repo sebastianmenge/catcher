f856566fd0e49aa0cafe0b1291dd3cf7486d9fa9
sebastianmenge <s.menge@gmx.net>
2013-06-26 01:12:15 +0200

    add better url scraping

--- app/controllers/api/entries_controller.rb
+++ app/controllers/api/entries_controller.rb
@@ -3,35 +3,59 @@ module Api
     require 'nokogiri'
     require 'open-uri'
 
-    def extract_url
+    def extract_props
       url = params[:url]
       doc = Nokogiri::HTML(open(url))
-      data = if fb_attrs(doc).present?
-        fb_attrs(doc)
+      render json: { properties: {
+        title: title(doc).first,
+        image: image(doc).first,
+        description: desc(doc).first,
+        url: url,
+        favicon: favicon(url)
+        }
+      }
+    end
+
+    private
+
+    def parse_prop(doc, prop)
+      doc.xpath("#{prop}").map do |node|
+        node.to_s
+      end
+    end
+
+    def title(doc)
+      fb = "//meta[@property='og:title']/@content"
+      p = '//title/text()'
+      if parse_prop(doc, fb).first.present?
+        parse_prop(doc, fb)
       else
-        substitute_attrs(doc)
+        parse_prop(doc, p)
       end
-      render json: {properties: data}
     end
 
-    def fb_attrs(doc, url)
-      props = ['title', 'image', 'description', 'url' ]
-      props = props.map do |prop|
-        doc.xpath("//meta[@property='og:#{prop}']/@content").map do |node|
-          node.to_s
-        end
+    def image(doc)
+      fb = "//meta[@property='og:image']/@content"
+      p = '//img/@src'
+      if parse_prop(doc, fb).first.present?
+        parse_prop(doc, fb)
+      else
+        parse_prop(doc, p)
       end
-      props.flatten if props.first.present?
     end
 
-    def substitute_attrs(doc, url)
-      props = ['//title/text()', '//img/@src', '//p/text()']
-      props = props.map do |prop|
-        doc.xpath("#{prop}").map do |node|
-          node.to_s
-        end
+    def desc(doc)
+      fb = "//meta[@property='og:description']/@content"
+      p = '//p/text()'
+      if parse_prop(doc, fb).first.present?
+        parse_prop(doc, fb)
+      else
+        parse_prop(doc, p)
       end
-      props << url
+    end
+
+    def favicon(url)
+      "http://www.google.com/s2/favicons?domain=#{url}"
     end
   end
 end
